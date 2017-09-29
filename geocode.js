2fd2e81b9b6ca4502a7927e947e884ea0b60deed
Jake Bowers <jwbowers@users.noreply.github.com>
2017-09-29 15:26:35 -0500

    Update geocode.js

--- geocode.js
+++ geocode.js
@@ -1,16 +1,20 @@
 Qualtrics.SurveyEngine.addOnload(function() {
-    this.questionclick = function(event, element) {
-        var postalcode = this.getTextInput();
+	this.disableNextButton();
+	var container = this;
+	var done = $j("<a>done</a>");
+	done.click(function(e) {
+		var address = container.getTextValue();
 
         var geocoder = new google.maps.Geocoder();
-        geocoder.geocode({componentRestrictions: {country: "Chile", postalCode: postalcode}}, function(results, status) {
+		geocoder.geocode({address: address, componentRestrictions: {country: "Chile"}}, function(results, status) {
             if (status == 'OK') {
                 Qualtrics.SurveyEngine.setEmbeddedData("lat", results[0].geometry.location.lat());
                 Qualtrics.SurveyEngine.setEmbeddedData("lon", results[0].geometry.location.lng());
-                Qualtrics.SurveyEngine.clickNextButton();
+                container.clickNextButton();
             } else {
-                Qualtrics.SurveyEngine.displayErrorMessage("Unable to process your postal code. Please try again.")
+                container.displayErrorMessage("Unable to process your postal code. Please try again.");
             }
         });
-    };
+	});	
+	$j(this.getQuestionContainer()).append(done);
 });
